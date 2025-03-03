23df51d93fc48ca4bdb156651cb47dc95e4d39ff
Geovanny-Quintero-Velez <geovannyalexander07@gmail.com>
2025-03-03 17:15:51 -0500

    polish swagger documentation

--- src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
+++ src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
@@ -3,6 +3,9 @@ package co.analisys.biblioteca.controller;
 import co.analisys.biblioteca.dto.NotificacionDTO;
 import co.analisys.biblioteca.service.NotificacionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +18,14 @@ public class NotificacionController {
 
     @Operation(
         summary = "Enviar notificación",
-        description = "Este endpoint permite enviar una notificación a un usuario específico. " +
+        description = "Permite enviar una notificación a un usuario específico. " +
                       "El contenido de la notificación se recibe en formato JSON como un objeto NotificacionDTO."
     )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Notificación enviada exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Solicitud inválida"),
+        @ApiResponse(responseCode = "403", description = "Acceso no autorizado")
+    })
     @PostMapping
     @PreAuthorize("hasRole('ROLE_EDITAR')")
     public void enviarNotificacion(@RequestBody NotificacionDTO notificacion) {
