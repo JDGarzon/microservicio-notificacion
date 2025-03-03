1ad29321dbf193eb0b0847c3ae02f4dc73fcb944
Geovanny-Quintero-Velez <geovannyalexander07@gmail.com>
2025-03-03 16:41:06 -0500

    Configure keyclock connection

--- src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
+++ src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
@@ -3,7 +3,7 @@ package co.analisys.biblioteca.controller;
 import co.analisys.biblioteca.dto.NotificacionDTO;
 import co.analisys.biblioteca.service.NotificacionService;
 import io.swagger.v3.oas.annotations.Operation;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +19,7 @@ public class NotificacionController {
                       "El contenido de la notificación se recibe en formato JSON como un objeto NotificacionDTO."
     )
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_EDITAR')")
     public void enviarNotificacion(@RequestBody NotificacionDTO notificacion) {
         notificacionService.enviarNotificacion(notificacion);
     }
